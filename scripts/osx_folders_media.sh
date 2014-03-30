bb8f34d094863ce624d344ab6fcfc8ee21a7d2b0
ajongsma <a.jongsma@gmail.com>
2014-03-30 17:55:53 +0200

    Update osx_folders_media.sh

--- scripts/osx_folders_media.sh
+++ scripts/osx_folders_media.sh
@@ -12,9 +12,6 @@ echo "#-------------------------------------------------------------------------
 
 [ -d $INST_FOLDER_USENET_WATCH ] || mkdir -p $INST_FOLDER_USENET_WATCH
 [ -d $INST_FOLDER_USENET_INCOMPLETE ] || mkdir -p $INST_FOLDER_USENET_INCOMPLETE
-[ -d $INST_FOLDER_USENET_COMPLETE ] || mkdir -p $INST_FOLDER_USENET_COMPLETE
-[ -d $INST_FOLDER_SERIES_COMPLETE ] || mkdir -p $INST_FOLDER_SERIES_COMPLETE
-[ -d $INST_FOLDER_MOVIES_COMPLETE ] || mkdir -p $INST_FOLDER_MOVIES_COMPLETE
 
 echo "#------------------------------------------------------------------------------"
 echo "# Creating Media folders - Complete"
