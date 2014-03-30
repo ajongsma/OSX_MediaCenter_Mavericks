aff228e77845b136303a90529457ec32c5c452c8
ajongsma <a.jongsma@gmail.com>
2014-03-30 22:27:24 +0200

    Create osx_cpan.sh

---
+++ scripts/osx_cpan.sh
@@ -0,0 +1,12 @@
+
+##https://forums.plex.tv/index.php/topic/72552-plexwatch-plex-notify-script-send-push-alerts-on-new-sessions-and-stopped/page-7
+
+cpan
+
+sudo cpan install Time::Duration
+sudo cpan install Time::ParseDate
+sudo cpan install LWP::UserAgent
+sudo cpan install XML::Simple
+sudo cpan install DBI
+sudo cpan install JSON
+#sudo cpan install Growl::GNTP
