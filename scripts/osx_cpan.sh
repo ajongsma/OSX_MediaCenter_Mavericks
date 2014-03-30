a28950f9822c84fd5a36286d54072d049624fabc
ajongsma <a.jongsma@gmail.com>
2014-03-30 22:28:22 +0200

    Update osx_cpan.sh

--- scripts/osx_cpan.sh
+++ scripts/osx_cpan.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/env bash
 
+if [ ! -f config.sh ]; then
+  source ../config.sh
+else
+  source config.sh
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing CPAN plus modules"
+echo "#------------------------------------------------------------------------------"
 ##https://forums.plex.tv/index.php/topic/72552-plexwatch-plex-notify-script-send-push-alerts-on-new-sessions-and-stopped/page-7
 
 cpan
@@ -10,3 +20,7 @@ sudo cpan install XML::Simple
 sudo cpan install DBI
 sudo cpan install JSON
 #sudo cpan install Growl::GNTP
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing CPAN plus modules - Complete"
+echo "#------------------------------------------------------------------------------"
