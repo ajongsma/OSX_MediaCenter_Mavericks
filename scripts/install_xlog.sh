b5b68fb32c42fbd04b66bfddaa1d4238da6dabee
ajongsma <a.jongsma@gmail.com>
2014-03-30 16:07:04 +0200

    Create install_xlog.sh

---
+++ scripts/install_xlog.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ ! -f config.sh ]; then
+  source ../config.sh
+else
+  source config.sh
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing XLog"
+echo "#------------------------------------------------------------------------------"
+
+if [ ! -e /Applications/xlog.app ] ; then
+  printf "$PRINTF_MASK" "XLog not installed, please install..." "$RED" "[FAIL]" "$RESET"
+  open https://itunes.apple.com/nl/app/xlog/id430304898?l=en&mt=12
+  while ( [ ! -e /Applications/Server.app ] )
+  do
+    printf "$PRINTF_MASK" "Waiting for XLog to be installed…" "$YELLOW" "[WAIT]" "$RESET"
+    sleep 15
+  done
+  
+  printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
+  open /Applications/Server.app
+  
+  echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+  read -n 1 -s
+else
+  printf "$PRINTF_MASK" "XLOG found" "$GREEN" "[OK]" "$RESET"
+fi
+
