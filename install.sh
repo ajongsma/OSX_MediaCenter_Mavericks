1631973ef229cdf18971729dba75211509c36061
ajongsma <a.jongsma@gmail.com>
2014-03-30 17:25:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -144,3 +144,22 @@ fi
 if [[ $INSTALL_OSX_SERVER == "true" ]]; then
   source "$DIR/scripts/install_osx_server.sh"
 fi
+
+#------------------------------------------------------------------------------
+# Install Plex Server
+#------------------------------------------------------------------------------
+if [[ $INST_PLEX_SERVER == "true" ]]; then
+    if [ ! -e /Applications/Plex\ Media\ Server.app ] ; then
+        printf 'Plex Server not installed, installing…\n' "$RED" $col '[FAIL]' "$RESET"
+        source "$DIR/scripts/install_plex_server.sh"
+        while ( [ ! -e /Applications/Plex\ Media\ Server.app ] )
+        do
+            echo "Waiting for Plex Server to be installed..."
+            sleep 15
+        done
+    else
+        printf 'Plex Server found\n' "$GREEN" $col '[OK]' "$RESET"
+    fi
+else
+    printf "$PRINTF_MASK" "Plex Server excluded from install" "$GREY" "[SKIP]" "$RESET"
+fi
