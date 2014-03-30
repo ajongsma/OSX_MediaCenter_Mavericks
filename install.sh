e67bf01b7c87aa010f2cd63991c362543d499cfc
ajongsma <a.jongsma@gmail.com>
2014-03-30 15:50:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -108,17 +108,17 @@ fi
 #------------------------------------------------------------------------------
 #chflags nohidden ~/Library
 if [[ $ENABLE_LIBRARY_UNHIDE == "true" ]]; then
-    echo "---------------------------------------------------"
-    echo "| Unhiding hidden Library folder                  |"
-    echo "---------------------------------------------------"
-    chflags nohidden ~/Library
+  echo "---------------------------------------------------"
+  echo "| Unhiding hidden Library folder                  |"
+  echo "---------------------------------------------------"
+  chflags nohidden ~/Library
 fi
 
 #------------------------------------------------------------------------------
 # Enable Tap to Click for this user and for the login screen
 #------------------------------------------------------------------------------
 if [[ $ENABLE_MOUSE_TAPTOCLICK == "true" ]]; then
-    source "$DIR/scripts/osx_mouse_taptoclick_enable.sh"
+  source "$DIR/scripts/osx_mouse_taptoclick_enable.sh"
 fi
 
 #------------------------------------------------------------------------------
@@ -128,41 +128,6 @@ fi
 #------------------------------------------------------------------------------
 # Check for OS X Server 2.0
 #------------------------------------------------------------------------------
-if [[ $ENABLE_LIBRARY_UNHIDE == "true" ]]; then
-
-  if [ ! -e /Applications/Server.app ] ; then
-    printf "$PRINTF_MASK" "OS X Server not installed, please install..." "$RED" "[FAIL]" "$RESET"
-    open https://itunes.apple.com/nl/app/os-x-server/id537441259?mt=12
-    while ( [ ! -e /Applications/Server.app ] )
-    do
-        printf "$PRINTF_MASK" "Waiting for OS X Server to be installed…" "$YELLOW" "[WAIT]" "$RESET"
-        sleep 15
-    done
-    printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
-    printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
-    printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
-    open /Applications/Server.app
-    
-    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-    read -n 1 -s
-  else
-    printf "$PRINTF_MASK" "OS X Server found" "$GREEN" "[OK]" "$RESET"
-  fi
-  
-  SERVICE='httpd'
-  if ps ax | grep -v grep | grep $SERVICE > /dev/null ; then
-    printf "$PRINTF_MASK" "$SERVICE' is running" "$GREEN" "[OK]" "$RESET"
-  else
-    printf "$PRINTF_MASK" "$SERVICE' is not running" "$RED" "[FAIL]" "$RESET"
-    
-    printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
-    printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
-    printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
-    
-    echo -e "${BLUE} --- press any key to abort --- ${RESET}"
-    read -n 1 -s
-    
-    open /Applications/Server.app
-    exit 1
-  fi
+if [[ $INSTALL_OSX_SERVER == "true" ]]; then
+  source "$DIR/scripts/install_osx_server.sh"
 fi
