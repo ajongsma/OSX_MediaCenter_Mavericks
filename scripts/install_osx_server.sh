608e4fb97b71cac87b2d782effb435abf04cca04
ajongsma <a.jongsma@gmail.com>
2014-03-30 15:49:50 +0200

    Create install_osx_server.sh

---
+++ scripts/install_osx_server.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [ ! -f config.sh ]; then
+  source ../config.sh
+else
+  source config.sh
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install OS X Server"
+echo "#------------------------------------------------------------------------------"
+
+if [ ! -e /Applications/Server.app ] ; then
+  printf "$PRINTF_MASK" "OS X Server not installed, please install..." "$RED" "[FAIL]" "$RESET"
+  open https://itunes.apple.com/nl/app/os-x-server/id537441259?mt=12
+  while ( [ ! -e /Applications/Server.app ] )
+  do
+      printf "$PRINTF_MASK" "Waiting for OS X Server to be installed…" "$YELLOW" "[WAIT]" "$RESET"
+      sleep 15
+  done
+  printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
+  open /Applications/Server.app
+  
+  echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+  read -n 1 -s
+else
+  printf "$PRINTF_MASK" "OS X Server found" "$GREEN" "[OK]" "$RESET"
+fi
+
+SERVICE='httpd'
+if ps ax | grep -v grep | grep $SERVICE > /dev/null ; then
+  printf "$PRINTF_MASK" "$SERVICE' is running" "$GREEN" "[OK]" "$RESET"
+else
+  printf "$PRINTF_MASK" "$SERVICE' is not running" "$RED" "[FAIL]" "$RESET"
+  
+  printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
+  
+  echo -e "${BLUE} --- press any key to abort --- ${RESET}"
+  read -n 1 -s
+  
+  open /Applications/Server.app
+  exit 1
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install OS X Server - Complete"
+echo "#------------------------------------------------------------------------------"
