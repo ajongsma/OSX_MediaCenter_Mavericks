6b39ce250d6791c9ed506a38c49e47956a5bad8b
ajongsma <a.jongsma@gmail.com>
2014-03-30 23:55:20 +0200

    Update install_plex_server_plexwatch.sh

--- scripts/install_plex_server_plexwatch.sh
+++ scripts/install_plex_server_plexwatch.sh
@@ -28,9 +28,6 @@ printf "$PRINTF_MASK" "$server_log = ".$HOME."'/Library/Logs/Plex Media Server.l
 printf "$PRINTF_MASK" "$log_client_ip = 1;" "$YELLOW" "[WAIT]" "$RESET"
 printf "$PRINTF_MASK" "$myPlex_user = '".$MYPLEX_UID."';" "$YELLOW" "[WAIT]" "$RESET"
 printf "$PRINTF_MASK" "$myPlex_user = '".$MYPLEX_PW."';" "$YELLOW" "[WAIT]" "$RESET"
-
-
-printf "$PRINTF_MASK" "port   = 32400;" "$YELLOW" "[WAIT]" "$RESET"
 printf "$PRINTF_MASK" "-----------------------------------" "$YELLOW" "[WAIT]" "$RESET"
 echo -e "${BLUE} --- press any key to continue --- ${RESET}"
 read -n 1 -s
