692dac30ef5c28eb4885180ec01403e510bcda17
ajongsma <a.jongsma@gmail.com>
2014-03-30 16:11:37 +0200

    Update install_xlog.sh

--- scripts/install_xlog.sh
+++ scripts/install_xlog.sh
@@ -13,16 +13,16 @@ echo "#-------------------------------------------------------------------------
 if [ ! -e /Applications/xlog.app ] ; then
   printf "$PRINTF_MASK" "XLog not installed, please install..." "$RED" "[FAIL]" "$RESET"
   open https://itunes.apple.com/nl/app/xlog/id430304898?l=en&mt=12
-  while ( [ ! -e /Applications/Server.app ] )
+  while ( [ ! -e /Applications/Xlog.app ] )
   do
     printf "$PRINTF_MASK" "Waiting for XLog to be installed…" "$YELLOW" "[WAIT]" "$RESET"
     sleep 15
   done
   
-  printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
-  printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
-  printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
-  open /Applications/Server.app
+#  printf "$PRINTF_MASK" "Please enable:" "$YELLOW" "[WAIT]" "$RESET"
+#  printf "$PRINTF_MASK" "- Websites…" "$YELLOW" "[WAIT]" "$RESET"
+#  printf "$PRINTF_MASK" "- PHP Web Applications…" "$YELLOW" "[WAIT]" "$RESET"
+  open /Applications/Xlog.app
   
   echo -e "${BLUE} --- press any key to continue --- ${RESET}"
   read -n 1 -s
@@ -30,3 +30,6 @@ else
   printf "$PRINTF_MASK" "XLOG found" "$GREEN" "[OK]" "$RESET"
 fi
 
+echo "#------------------------------------------------------------------------------"
+echo "# Installing XLog - Complete"
+echo "#------------------------------------------------------------------------------"
