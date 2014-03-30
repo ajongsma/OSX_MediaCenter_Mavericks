04653358b25a53de2292419f4307346a29c2eaec
ajongsma <a.jongsma@gmail.com>
2014-03-30 20:43:43 +0200

    Create install_plex_plexwatch.sh

---
+++ scripts/install_plex_plexwatch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ ! -f config.sh ]; then
+  source ../config.sh
+else
+  source config.sh
+fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing PlexWatch"
+echo "#------------------------------------------------------------------------------"
+##https://github.com/ljunkie/plexWatch
+
+#if [ ! -e /Applications/iterm.app ] ; then
+#  printf "$PRINTF_MASK" "iTerm not installed, please install..." "$RED" "[FAIL]" "$RESET"
+#  open http://www.iterm2.com
+#  while ( [ ! -e /Applications/iterm.app ] )
+#  do
+#    printf "$PRINTF_MASK" "Waiting for iTerm to be installed…" "$YELLOW" "[WAIT]" "$RESET"
+#    sleep 15
+#  done
+#
+#  open /Applications/iterm.app
+#else
+#  printf "$PRINTF_MASK" "iTerm 2 found" "$GREEN" "[OK]" "$RESET"
+#fi
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing PlexWatch - Complete"
+echo "#------------------------------------------------------------------------------"
