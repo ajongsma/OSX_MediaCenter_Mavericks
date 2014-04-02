c6713f95dd53ad9b775d3a0489a56bba73c6bde4
ajongsma <a.jongsma@gmail.com>
2014-04-02 10:16:19 +0200

    Update install_plex_server_plexwatchweb.sh

--- scripts/install_plex_server_plexwatchweb.sh
+++ scripts/install_plex_server_plexwatchweb.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-#if [ ! -f config.sh ]; then
-#  source ../config.sh
-#else
-#  source config.sh
-#fi
 source ../config.sh
 #https://github.com/ecleese/plexWatchWeb.git
+## Requirements:
+## - Plex Media Server (v0.9.8+) and a PlexPass membership
+## - plexWatch (v0.1.6+)
+## - a web server that supports php (apache, nginx, XAMPP, WampServer, EasyPHP, lighttpd, etc)
+## - php5
+## - php5-sqlite
+## - php5-curl
+## - php5-json
 
-if [ ! -f $INST_PLEXWATCH_PATH/plexWatchWeb/index.php ] ; then
+if [ ! -f $INST_PLEXWATCHWEB_PATH/plexWatchWeb/index.php ] ; then
   echo "#------------------------------------------------------------------------------"
-  echo "# Installing PlexWatch/Web"
+  echo "# Installing PlexWatchWeb"
   echo "#------------------------------------------------------------------------------"
 else
 
@@ -21,7 +24,7 @@ if [ ! -d $INST_PLEXWATCHWEB_PATH ] ; then
   sudo mkdir -p $INST_PLEXWATCHWEB_PATH
   sudo chown `whoami`:staff $INST_PLEXWATCHWEB_PATH
 else
-  printf "$PRINTF_MASK" $INST_PLEXWATCH_PATH" exists" "$GREEN" "[OK]" "$RESET"
+  printf "$PRINTF_MASK" $INST_PLEXWATCHWEB_PATH" exists" "$GREEN" "[OK]" "$RESET"
 fi
 cd $INST_PLEXWATCHWEB_PATH
 
@@ -29,7 +32,7 @@ cd $INST_PLEXWATCHWEB_PATH
 
 ##################### TESTING #####################
 
-if [ ! -d $INST_PLEXWATCH_PATH/plexwatch ] ; then
+if [ ! -d $INST_PLEXWATCHWEB_PATH/plexwatch ] ; then
   printf "$PRINTF_MASK" "PlexWatch doesn't exists, downloading respository..." "$YELLOW" "[WAIT]" "$RESET"
   git clone https://github.com/ljunkie/plexWatch.git
   chmod 777 plexWatch
