ce53f8e951617c29bbba40d0dda87fd8bd78b124
ajongsma <a.jongsma@gmail.com>
2014-04-02 18:20:43 +0200

    Update install_plex_server_plexwatchweb.sh

--- scripts/install_plex_server_plexwatchweb.sh
+++ scripts/install_plex_server_plexwatchweb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-source ../config.sh
+source config.sh
+
 #https://github.com/ecleese/plexWatchWeb.git
 ## Requirements:
 ## - Plex Media Server (v0.9.8+) and a PlexPass membership
@@ -28,70 +29,29 @@ if [ ! -f $INST_PLEXWATCHWEB_PATH/plexWatchWeb/index.php ] ; then
     printf "$PRINTF_MASK" "PlexWatchWeb doesn't exists, downloading respository..." "$YELLOW" "[WAIT]" "$RESET"
     cd $INST_PLEXWATCHWEB_PATH
     git clone https://github.com/ecleese/plexWatchWeb.git
+    cd $DIR
   else
     printf "$PRINTF_MASK" "PlexWatchWeb exists" "$GREEN" "[OK]" "$RESET"
   fi
 
-if [ ! -d /Library/Server/Web/Data/Sites/Default ] ; then
-
-
-echo -e "${RED}-----------------------------------------------------------"
-echo -e "${RED}| ERROR"
-echo -e "${RED}-----------------------------------------------------------"
-echo -e "${RED}| OS X Server Webdirectory not found"
-echo -e "${RED}-----------------------------------------------------------"
-
-echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-read -n 1 -s
-exit 1
-
-
-
-  
-else
-
-sudo ln -s $INST_PLEXWATCHWEB_PATH/plexWatchWeb /Library/Server/Web/Data/Sites/Default/plexWatchWeb
-
+  if [ ! -d /Library/Server/Web/Data/Sites/Default ] ; then
+    echo -e "${RED}-----------------------------------------------------------"
+    echo -e "${RED}| ERROR"
+    echo -e "${RED}-----------------------------------------------------------"
+    echo -e "${RED}| OS X Server Webdirectory not found"
+    echo -e "${RED}-----------------------------------------------------------"
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+    exit 1
+  else
+    if [ -h /Library/Server/Web/Data/Sites/Default/plexWatchWeb ] ; then
+      printf "$PRINTF_MASK" "Symbolic link to PlexWatchWeb exist" "$GREEN" "[OK]" "$RESET"
+    else
+      printf "$PRINTF_MASK" "Symbolic link to PlexWatchWeb doesn't exit, creating..." "$YELLOW" "[WAIT]" "$RESET"
+      sudo ln -s $INST_PLEXWATCHWEB_PATH/plexWatchWeb /Library/Server/Web/Data/Sites/Default/plexWatchWeb
+    fi
+  fi
+  echo "#------------------------------------------------------------------------------"
+  echo "# Installing PlexWatchWeb - Complete"
+  echo "#------------------------------------------------------------------------------"
 fi
-
-
-
-
-
-
-
-##################### TESTING #####################
-
-
-
-cd plexWatch
-if [ ! -f config.pl ] ; then
-  printf "$PRINTF_MASK" "PlexWatch config.pl doesn't exists, copying..." "$YELLOW" "[WAIT]" "$RESET"
-  cp config.pl-dist config.pl
-else
-  printf "$PRINTF_MASK" "config.pl exists" "$GREEN" "[OK]" "$RESET"
-fi
-
-printf "$PRINTF_MASK" "Modify Variables as needed" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "\$data_dir = '/Users/Plex/plexWatch';" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "\$server_log = '"$HOME"/Library/Logs/Plex Media Server.log';" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "\$log_client_ip = 1;" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "\$myPlex_user = <Plex Username>';" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "\$myPlex_user = <Plex Password>';" "$GREY" "|" "$RESET"
-printf "$PRINTF_MASK" "-----------------------------------" "$GREY" "|" "$RESET"
-echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-read -n 1 -s
-open -a /Applications/TextWrangler.app config.pl
-
-if [ ! -f $HOME/Library/LaunchAgents/com.plexserver.plexwatch.plist ] ; then
-  printf "$PRINTF_MASK" "com.plexserver.plexwatch.plist doesn't exists, copying..." "$YELLOW" "[WAIT]" "$RESET"
-  cp ./config/launchctl/com.plexserver.plexwatch.plist $HOME/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/com.plexserver.plexwatch.plist
-else
-  printf "$PRINTF_MASK" "com.plexserver.plexwatch.plist exists" "$GREEN" "[OK]" "$RESET"
-fi
-
-cd $DIR
-echo "#------------------------------------------------------------------------------"
-echo "# Installing PlexWatch - Complete"
-echo "#------------------------------------------------------------------------------"
