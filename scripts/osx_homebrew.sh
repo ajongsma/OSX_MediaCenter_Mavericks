0890fee733d6b6fbbb452d8636ba3213a699edd8
ajongsma <a.jongsma@gmail.com>
2014-04-04 21:58:17 +0200

    Update osx_homebrew.sh

--- scripts/osx_homebrew.sh
+++ scripts/osx_homebrew.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
-if [ ! -f config.sh ]; then
-  source ../config.sh
+source config.sh
+ printf "$PRINTF_MASK" "Homebrew detected" "$GREEN" "[OK]" "$RESET"
+if ! which brew &>/dev/null; then
+
 else
-  source config.sh
+  printf "$PRINTF_MASK" "Homebrew not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
+  ruby <(curl -fsS https://raw.github.com/Homebrew/homebrew/go/install)
+
+  if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
+    fancy_echo "Put Homebrew location earlier in PATH ..."
+      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
+      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
+      export PATH="/usr/local/bin:$PATH"
+  fi
 fi
+
+
+##### TESTING #####
+exit 0
+##### TESTING #####
 
 echo "#------------------------------------------------------------------------------"
 echo "# Installing HomeBrew"
