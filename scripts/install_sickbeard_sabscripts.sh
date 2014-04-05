0c61334c9432f62b792cef1ab038a9d23d40f4a8
ajongsma <a.jongsma@gmail.com>
2014-04-05 17:25:28 +0200

    Update install_sickbeard_sabscripts.sh

--- scripts/install_sickbeard_sabscripts.sh
+++ scripts/install_sickbeard_sabscripts.sh
@@ -2,14 +2,40 @@
 source config.sh
 
 if [ -f ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg ] ; then
-  printf "$PRINTF_MASK" "-> SickBeard to SABnzbd config detected\n" "$GREEN" "[OK]" "$RESET"
-#  check_config_defaults
-#  check_config_var
+  printf "$PRINTF_MASK" "-> SickBeard to SABnzbd config detected" "$GREEN" "[OK]" "$RESET"
 else
-  printf "$PRINTF_MASK" "-> SickBeard to SABnzbd config not detected, installing...\n" "$YELLOW" "[WAIT]" "$RESET"
-#  check_config_defaults
+  printf "$PRINTF_MASK" "-> SickBeard to SABnzbd config not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
 
-
+  if [ -f ~/Library/Application\ Support/SABnzbd/scripts/sabToSickBeard.py ] ; then
+    printf 'Sickbeard default SABnzbd scripts installed\n' "$GREEN" $col '[OK]' "$RESET"
+  else
+    printf 'Sickbeard default SABnzbd scripts installed not installed\n' "$RED" $col '[FAIL]' "$RESET"
+    cp -iv /Applications/Sick-Beard/autoProcessTV/* ~/Library/Application\ Support/SABnzbd/scripts/
+  fi
+  
+  if [ -f ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg ] ; then
+    printf 'Sickbeard config file installed\n' "$GREEN" $col '[OK]' "$RESET"
+  else
+    printf 'Sickbeard config file scripts installed not installed\n' "$RED" $col '[FAIL]' "$RESET"
+    cp -iv /Applications/Sick-Beard/autoProcessTV/* ~/Library/Application\ Support/SABnzbd/scripts/
+      
+      
+    echo "-----------------------------------------------------------"
+    echo "| Add the following to [SickBeard]:"
+    echo "| host                                    : localhost"
+    echo "| Port                                    : 8081"
+    echo "| username                                : $INST_SICKBEARD_UID"
+    echo "| password                                : $INST_SICKBEARD_PW"
+    echo "-----------------------------------------------------------"
+    if [ ! -d /Applications/TextWrangler.app ]; then
+      pico ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg
+    else
+      open -a /Applications/TextWrangler.app ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg
+    fi
+    
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+  fi
 
 fi
 
@@ -25,25 +51,8 @@ echo "#-------------------------------------------------------------------------
 
 
 
-if [ -f ~/Library/Application\ Support/SABnzbd/scripts/sabToSickBeard.py ] ; then
-    printf 'Sickbeard default SABnzbd scripts installed\n' "$GREEN" $col '[OK]' "$RESET"
-else
-    printf 'Sickbeard default SABnzbd scripts installed not installed\n' "$RED" $col '[FAIL]' "$RESET"
 
-    cp -iv /Applications/Sick-Beard/autoProcessTV/* ~/Library/Application\ Support/SABnzbd/scripts/
-    cp -iv ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg.sample ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg
-fi
 
-echo "-----------------------------------------------------------"
-echo "| Add the following to [SickBeard]:"
-echo "| host                                    : localhost"
-echo "| Port                                    : 8081"
-echo "| username                                : $INST_SICKBEARD_UID"
-echo "| password                                : $INST_SICKBEARD_PW"
-echo "-----------------------------------------------------------"
-subl ~/Library/Application\ Support/SABnzbd/scripts/autoProcessTV.cfg
-echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-read -n 1 -s
 
 
 echo "#------------------------------------------------------------------------------"
