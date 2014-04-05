727d1628a4560b396b96d1ec381e83d5603b6c03
ajongsma <a.jongsma@gmail.com>
2014-04-05 16:06:05 +0200

    Create install_cheetah.sh

---
+++ scripts/install_cheetah.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+source config.sh
+
+
+
+
+
+##### TESTING #####
+exit 0
+##### TESTING #####
+
+echo "#------------------------------------------------------------------------------"
+echo "# Installing Cheeta"
+echo "#------------------------------------------------------------------------------"
+
+
+
+echo "Download latest Cheetah from http://www.cheetahtemplate.org/download"
+open http://www.cheetahtemplate.org/download
+
+cd ~/Downloads
+#curl -O http://sphinxsearch.com/files/sphinx-2.0.6-release.tar
+while ( [ ! -e Cheetah-2.4.4.tar ] )
+do
+    printf 'Waiting for Cheetah to be downloaded…\n' "YELLOW" $col '[WAIT]' "$RESET"
+    sleep 15
+done
+
+tar xvzf Cheetah-2.4.4.tar
+cd Cheetah-2.4.4
+sudo python setup.py install
+
+echo "#------------------------------------------------------------------------------"
+echo "# Install Cheeta - Complete"
+echo "#------------------------------------------------------------------------------"
