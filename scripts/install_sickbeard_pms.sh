f19777d1245d60727ff27f1d373200f6d3c0e8d1
ajongsma <a.jongsma@gmail.com>
2014-04-05 19:08:10 +0200

    Create install_sickbeard_pms.sh

---
+++ scripts/install_sickbeard_pms.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+source config.sh
+
+printf "$PRINTF_MASK" "-> Configuring SickBeard to Plex Media Server..." "$YELLOW" "[WAIT]" "$RESET"
+
+echo "-----------------------------------------------------------"
+echo "| Plex Media Server     : Enable"
+echo "| - Notify on Download  : Enable"
+echo "| - Update Library      : Enable"
+echo "| - Server IP:Port      : 127.0.0.1:32400"
+echo "| - Client IP:Port      : 127.0.0.1:3000"
+echo "| - Plex Client Username: ??"
+echo "| - Plex Client Password: ??"
+echo "-----------------------------------------------------------"
+echo "| Test, Save"
+echo "-----------------------------------------------------------"
+open http://pooky.local:8081/config/notifications/
+echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+read -n 1 -s
+
+printf "$PRINTF_MASK" "-> Configuring SABnzbd to SickBeard - Complete" "$GREEN" "[OK]" "$RESET"
