1f7d7af67288ef355590f35c09f930e276f4aacd
ajongsma <a.jongsma@gmail.com>
2014-04-05 20:06:45 +0200

    Update install_sickbeard_trakttv.sh

--- scripts/install_sickbeard_trakttv.sh
+++ scripts/install_sickbeard_trakttv.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 source config.sh
 
+function check_config_defaults() {
+  if [[ -z $INST_TRAKT_UID ]] || [[ -z $INST_TRAKT_PW ]] || [[ -z $INST_TRAKT_KEY_API ]]; then
+    printf 'One or more values were not detected in the config.sh, please add the appropriate values:\n' "YELLOW" $col '[WAIT]' "$RESET"
+    echo "| SABNzbd username  : INST_TRAKT_UID"
+    echo "| SABnzbd password  : INST_TRAKT_PW"
+    echo "| SABnzbd API Key   : INST_TRAKT_KEY_API"
+    if [ ! -d /Applications/TextWrangler.app ]; then
+      pico config.sh
+    else
+      open -a /Applications/TextWrangler.app config.sh
+    fi
+    while ( [[ $INST_TRAKT_UID == "" ]] || [[ $INST_TRAKT_PW == "" ]] || [[ $INST_TRAKT_KEY_API == "" ]] )
+    do
+      printf '.'
+      sleep 2
+      source config.sh
+    done
+    printf "$PRINTF_MASK" ".\n" "$GREEN" "[OK]" "$RESET"
+  fi
+}
+
+check_config_defaults
 printf "$PRINTF_MASK" "-> Configuring SickBeard to TraktTV..." "$YELLOW" "[WAIT]" "$RESET"
 echo "-----------------------------------------------------------"
 echo "| SickBeard -> Config -> Notifications"
