33dcefad28d166b1cf340c7cf50ef7725b75b5a9
ajongsma <a.jongsma@gmail.com>
2014-04-05 17:43:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -204,5 +204,6 @@ if [[ $INST_SICKBEARD == "true" ]]; then
   source "$DIR/scripts/install_cheetah.sh"
   source "$DIR/scripts/install_sickbeard.sh"
   source "$DIR/scripts/install_sickbeard_sabscripts.sh"
+  source "$DIR/scripts/install_sabnzbd_sickbeard.sh"
 fi
 
