1a60c45ac0ca3b3f9f299f99a67fe036e57bed23
ajongsma <a.jongsma@gmail.com>
2014-04-06 14:23:12 +0200

    Update install_autosub.sh

--- scripts/install_autosub.sh
+++ scripts/install_autosub.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 source config.sh
 
 if [ ! -e /usr/local/bin/brew ] ; then
@@ -8,34 +7,68 @@ if [ ! -e /usr/local/bin/brew ] ; then
   exit 1
 fi
 
-if [ -e /usr/local/bin/hg ] ; then
-  printf "$PRINTF_MASK" "HG detected" "$GREEN" "[OK]" "$RESET"
-else
-  printf "$PRINTF_MASK" "HG not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
-  brew install hg
-fi
-
 if [ -d /Applications/auto-sub ] ; then
   printf "$PRINTF_MASK" "Auto-Sub detected" "$GREEN" "[OK]" "$RESET"
 else
   printf "$PRINTF_MASK" "Auto-Sub not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
+  
+  if [ -e /usr/local/bin/hg ] ; then
+    printf "$PRINTF_MASK" "HG detected" "$GREEN" "[OK]" "$RESET"
+  else
+    printf "$PRINTF_MASK" "HG not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
+    brew install hg
+  fi
+  
+  cd /Applications
+  hg clone https://code.google.com/p/auto-sub/
+  sudo chown `whoami`:wheel -R /Applications/auto-sub
+  
+  echo "-----------------------------------------------------------"
+  echo "| Click main menu item Config (niet sub-menu item(s)), General:"
+  echo "| Rootpath            : /Users/Andries/Media/Series"
+  echo "| Launchbrowser       : Disabled"
+  echo "| Fallback to English : Disabled"
+  echo "| Subtitle English    : nl"
+  echo "| Notify English      : Disabled"
+  echo "| Notify Dutch        : Disabled"
+  echo "-----------------------"
+  echo "| Save"
+  echo "-----------------------------------------------------------"
+  osascript -e 'tell app "Terminal"
+      do script "python /Applications/auto-sub/AutoSub.py"
+  end tell'
+  #sudo python /Applications/auto-sub/AutoSub.py
+  echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+  read -n 1 -s
+  
+  
+  INST_FILE_LAUNCHAGENT="com.autosub.autosub.plist"
+  if [ -f ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT ] ; then
+    printf "$PRINTF_MASK" "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT" "$YELLOW" "[WAIT]" "$RESET"
+    cp $DIR/launchctl/config/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
+    if [ "$?" != "0" ]; then
+      echo -e "${RED}  ============================================== ${RESET}"
+      echo -e "${RED} | ERROR ${RESET}"
+      echo -e "${RED} | Copy failed: ${RESET}"
+      echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+      echo -e "${RED} | --- press any key to continue --- ${RESET}"
+      echo -e "${RED}  ============================================== ${RESET}"
+      read -n 1 -s
+      exit 1
+    fi
+    launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
+  else
+    printf "$PRINTF_MASK" "Launch Agent detected" "$GREEN" "[OK]" "$RESET"
+  fi
+
+  cd $DIR
+  printf "$PRINTF_MASK" "Auto-Sub installed" "$GREEN" "[OK]" "$RESET"
 fi
-
-
 
 ##### TESTING #####
 exit 0
 ##### TESTING #####
 
-cd /Applications
-hg clone https://code.google.com/p/auto-sub/
-sudo chown `whoami`:wheel -R /Applications/auto-sub
-
-
-
-echo "#------------------------------------------------------------------------------"
-echo "# Installing Auto-Sub"
-echo "#------------------------------------------------------------------------------"
 #AutoSub.py  ExamplePostProcess.py  README.txt  autosub changelog.xml  cherrypy  init.ubuntu  interface  library
 
 #echo "Download latest Auto-Sub from http://code.google.com/p/auto-sub/"
@@ -48,55 +81,3 @@ echo "#-------------------------------------------------------------------------
 #done
 #sleep 3
 #sudo mv ~/Downloads/auto-sub /Applications/
-
-
-
-echo "-----------------------------------------------------------"
-echo "| Click main menu item Config (niet sub-menu item(s)), General:"
-echo "| Rootpath            : /Users/Andries/Media/Series"
-echo "| Launchbrowser       : Disabled"
-echo "| Fallback to English : Disabled"
-echo "| Subtitle English    : nl"
-echo "| Notify English      : Disabled"
-echo "| Notify Dutch        : Disabled"
-echo "-----------------------"
-echo "| Save"
-echo "-----------------------------------------------------------"
-
-cd /Applications/auto-sub
-osascript -e 'tell app "Terminal"
-    do script "python /Applications/auto-sub/AutoSub.py"
-end tell'
-#sudo python /Applications/auto-sub/AutoSub.py
-echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-read -n 1 -s
-
-INST_FILE_LAUNCHAGENT="com.autosub.autosub.plist"
-if [ -f $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT ] ; then
-    echo "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT"
-    cp $DIR/launchctl/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
-    if [ "$?" != "0" ]; then
-        echo -e "${RED}  ============================================== ${RESET}"
-        echo -e "${RED} | ERROR ${RESET}"
-        echo -e "${RED} | Copy failed: ${RESET}"
-        echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
-        echo -e "${RED} | --- press any key to continue --- ${RESET}"
-        echo -e "${RED}  ============================================== ${RESET}"
-        read -n 1 -s
-        exit 1
-    fi
-else
-    echo -e "${RED}  ============================================== ${RESET}"
-    echo -e "${RED} | ERROR ${RESET}"
-    echo -e "${RED} | LaunchAgent file not found: ${RESET}"
-    echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
-    echo -e "${RED} | --- press any key to continue --- ${RESET}"
-    echo -e "${RED}  ============================================== ${RESET}"
-    read -n 1 -s
-    sudo mv /tmp/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
-fi
-launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
-
-echo "#------------------------------------------------------------------------------"
-echo "# Install Auto-Sub - Complete"
-echo "#------------------------------------------------------------------------------"
