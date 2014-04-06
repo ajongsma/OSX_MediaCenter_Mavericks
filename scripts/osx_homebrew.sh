226ac19c8b9a66e3a9c83d68f5417c4c7702185a
ajongsma <a.jongsma@gmail.com>
2014-04-06 10:58:10 +0200

    Update osx_homebrew.sh

--- scripts/osx_homebrew.sh
+++ scripts/osx_homebrew.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 source config.sh
- printf "$PRINTF_MASK" "Homebrew detected" "$GREEN" "[OK]" "$RESET"
+ 
 if ! which brew &>/dev/null; then
-
+ printf "$PRINTF_MASK" "Homebrew detected" "$GREEN" "[OK]" "$RESET"
 else
   printf "$PRINTF_MASK" "Homebrew not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
   ruby <(curl -fsS https://raw.github.com/Homebrew/homebrew/go/install)
@@ -15,7 +15,16 @@ else
       export PATH="/usr/local/bin:$PATH"
   fi
   printf "$PRINTF_MASK" "Updating Homebrew formulas..." "$YELLOW" "[WAIT]" "$RESET"
+  # Make sure we’re using the latest Homebrew
   brew update
+  
+  # Upgrade any already-installed formulae
+  brew upgrade
+  
+  brew doctor
+  
+  # Remove outdated versions from the cellar
+  brew cleanup
 fi
 
 
@@ -38,6 +47,8 @@ if ! which brew &>/dev/null; then
       printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
       export PATH="/usr/local/bin:$PATH"
   fi
+  
+  
 else
   fancy_echo "Homebrew already installed. Skipping ..."
 fi
@@ -49,3 +60,25 @@ echo "#-------------------------------------------------------------------------
 echo "# Installing HomeBrew - Complete"
 echo "#------------------------------------------------------------------------------"
 
+#### SOME OTHER SCRIPT
+
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+if [ ! -e /usr/local/bin/brew ] ; then
+    printf 'HomeBrew failed installing\n' "$RED" $col '[FAIL]' "$RESET"
+    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+    read -n 1 -s
+    exit 1
+else
+    printf 'HomeBrew found\n' "$GREEN" $col '[OK]' "$RESET"
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew doctor
+
+# Remove outdated versions from the cellar
+brew cleanup
