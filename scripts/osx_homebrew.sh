e60e99bf9edbad293377edc9d40af9c0cd4042fb
ajongsma <a.jongsma@gmail.com>
2014-04-06 11:41:50 +0200

    Update osx_homebrew.sh

--- scripts/osx_homebrew.sh
+++ scripts/osx_homebrew.sh
@@ -20,66 +20,17 @@ else
   brew update
   
   # Upgrade any already-installed formulae
+  printf "$PRINTF_MASK" "Upgrading Homebrew formulas..." "$YELLOW" "[WAIT]" "$RESET"
   brew upgrade
   
+  printf "$PRINTF_MASK" "Checking OS X Homebrew configuration..." "$YELLOW" "[WAIT]" "$RESET"
   brew doctor
   
   # Remove outdated versions from the cellar
+  printf "$PRINTF_MASK" "Cleaning up..." "$YELLOW" "[WAIT]" "$RESET"
   brew cleanup
-fi
-
-
-##### TESTING #####
-exit 0
-##### TESTING #####
-
-echo "#------------------------------------------------------------------------------"
-echo "# Installing HomeBrew"
-echo "#------------------------------------------------------------------------------"
-#http://brew.sh
-
-if ! which brew &>/dev/null; then
-  fancy_echo "Installing Homebrew, a good OS X package manager ..."
-    ruby <(curl -fsS https://raw.github.com/Homebrew/homebrew/go/install)
-
-  if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
-    fancy_echo "Put Homebrew location earlier in PATH ..."
-      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
-      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
-      export PATH="/usr/local/bin:$PATH"
+  
+  if [ -e /usr/local/bin/brew ] ; then
+    printf "$PRINTF_MASK" "Homebrew installed" "$GREEN" "[OK]" "$RESET"
   fi
-  
-  
-else
-  fancy_echo "Homebrew already installed. Skipping ..."
 fi
-
-fancy_echo "Updating Homebrew formulas ..."
-brew update
-
-echo "#------------------------------------------------------------------------------"
-echo "# Installing HomeBrew - Complete"
-echo "#------------------------------------------------------------------------------"
-
-#### SOME OTHER SCRIPT
-
-ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
-if [ ! -e /usr/local/bin/brew ] ; then
-    printf 'HomeBrew failed installing\n' "$RED" $col '[FAIL]' "$RESET"
-    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-    read -n 1 -s
-    exit 1
-else
-    printf 'HomeBrew found\n' "$GREEN" $col '[OK]' "$RESET"
-fi
-
-# Make sure we’re using the latest Homebrew
-brew update
-
-# Upgrade any already-installed formulae
-brew upgrade
-
-brew doctor
-
-# Remove outdated versions from the cellar
-brew cleanup
