dc36c1d14c07c1e7aedd5fe46ffc8562f1dd7a38
ajongsma <a.jongsma@gmail.com>
2014-04-07 18:20:18 +0200

    Update install_mysql.sh

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 source config.sh
 
-if [ -e /usr/local/bin/brew ] ; then
- printf "$PRINTF_MASK" "Homebrew detected" "$GREEN" "[OK]" "$RESET"
-else
-  printf "$PRINTF_MASK" "Homebrew not detected" "$RED" "[ERR]" "$RESET"
+if [ ! -e /usr/local/bin/brew ] ; then
+ printf "$PRINTF_MASK" "Homebrew not detected" "$RED" "[ERR]" "$RESET"
   exit 1
 fi
 
 if [ ! -e /usr/local/bin/mysql ] ; then
- printf "$PRINTF_MASK" "Homebrew detected, updating..." "$YELLOW" "[WAIT]" "$RESET"
+  printf "$PRINTF_MASK" "Homebrew detected, updating..." "$YELLOW" "[WAIT]" "$RESET"
   brew update
   brew doctor
   brew upgrade
