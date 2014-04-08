1b36485871f336ef849a2946b483a47306a9ca51
ajongsma <a.jongsma@gmail.com>
2014-04-08 20:53:31 +0200

    Update install_spotweb.sh

--- scripts/install_spotweb.sh
+++ scripts/install_spotweb.sh
@@ -94,7 +94,7 @@ if [ ! -h $INST_APACHE_SYSTEM_WEB_ROOT/spotweb ] ; then
   check_config_defaults
   check_config_var
 else
-  printf "$PRINTF_MASK" "-> Spotweb not detected, installing..." "$GREEN" "[OK]" "$RESET"
+  printf "$PRINTF_MASK" "-> Spotweb not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
   check_config_defaults
 
   if [ ! -d $INST_SPOTWEB_PATH ] ; then
@@ -157,61 +157,93 @@ else
     fi
   fi
 
+  echo "-----------------------------------------------------------"
+  echo "| Paste the information as seen in the installer:"
+  echo "| Type                          : MySQL"
+  echo "| Server                        : localhost"
+  echo "| Database                      : $INST_SPOTWEB_MYSQL_DB"
+  echo "| Username                      : $INST_SPOTWEB_MYSQL_UID"
+  echo "| Password                      : $INST_SPOTWEB_MYSQL_PW"
+  echo "-----------------------------------------------------------"
+  echo "| Usenet Server                 : $INST_NEWSSERVER_NAME"
+  echo "| User Name                     : $INST_NEWSSERVER_SERVER_UID"
+  echo "-----------------------------------------------------------"
+  open http://localhost/spotweb/install.php
+  echo " --- press any key to continue ---"
+  read -n 1 -s
+  
+  echo "-----------------------------------------------------------"
+  echo "| After finishing all steps, copy/paste all the information as shown during the last phase"
+  touch $INST_SPOTWEB_PATH/dbsettings.inc.php
+  if [ ! -d /Applications/TextWrangler.app ]; then
+    pico $INST_SPOTWEB_PATH/dbsettings.inc.php
+  else
+    open -a /Applications/TextWrangler.app $INST_SPOTWEB_PATH/dbsettings.inc.php
+  fi
+  echo " --- press any key to continue ---"
+  read -n 1 -s
 
+  echo "-----------------------------------------------------------"
+  echo "| Click on the Cogwheel top-right and enter the following settings:"
+  echo "| "
+  echo "| NZB Handeling"
+  echo "| What shall we do with NZB files           : Call SABnzbd through HTTP by Spotweb"
+  echo "| What shall we do with multiple NZB files? : Merge NZB files"
+  echo "| URL to SABnzbd                            : localhost:$INST_SABNZBD_PORT"
+  echo "| API key for SABnzbd                       : $INST_SABNZBD_KEY_API"
+  echo "-----------------------------------------------------------"
+  echo "| Change"
+  echo "-----------------------------------------------------------"
+  open http://localhost/spotweb/install.php
+  echo " --- press any key to continue ---"
+  read -n 1 -s
 
-
-#echo "OSX Server -> Websites"
-#echo "Select  : Server Website"
-#echo "Click   : Edit"
-#echo "Click   : Edit Advanced Settings"
-#echo "Allow overrides using .htaccess: Enable"
-#echo "Click   : OK"
-#echo "Click   : OK"
-#open /Applications/Server.app
-
-
-
-#################### 
-## http://www.macminivault.com/mysql-mavericks/
-## https://github.com/CiTroNaK/Installation-guide-for-GitLab-on-OS-X
-#################### 
-#/usr/bin/mysqladmin ping| grep 'mysqld is alive' > /dev/null 2>&1
-#if [ $? != 0 ]
-#then
-#  /etc/init.d/mysqld stop
-#  /etc/init.d/mysqld start
-#    echo Starting at `date`
-#fi
-#################### 
-#MYSQL_UP=$(pgrep mysql | wc -l);
-#if [ "$MYSQL_UP" -ne 1 ]; then
-#  echo "MySQL is down."
-#  #sudo service mysql start
-#else
-#  echo "MySQL is up."
-#fi
-#################### 
+  INST_FILE_LAUNCHAGENT="com.tmux.spotweb.plist"
+  if [ -f $DIR/config/launchctl/$INST_FILE_LAUNCHAGENT ] ; then
+    printf "$PRINTF_MASK" "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT" "$YELLOW" "[WAIT]" "$RESET"
+    cp $DIR/config/launchctl/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
+    if [ "$?" != "0" ]; then
+      echo -e "${RED}  ============================================== ${RESET}"
+      echo -e "${RED} | ERROR ${RESET}"
+      echo -e "${RED} | Copy failed: ${RESET}"
+      echo -e "${RED} | $DIR/config/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
+      echo -e "${RED} | --- press any key to continue --- ${RESET}"
+      echo -e "${RED}  ============================================== ${RESET}"
+      read -n 1 -s
+      exit 1
+    fi
+    launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
+  else
+    printf "$PRINTF_MASK" "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT" "$GREEN" "[OK]" "$RESET"
+  fi
+  
+  echo "-----------------------------------------------------------"
+  echo "| OSX Server -> Websites"
+  echo "-----------------------------------------------------------"
+  echo "| Select                                    : Server Website"
+  echo "| Click                                     : Edit"
+  echo "| Click                                     : Edit Advanced Settings"
+  echo "| Allow overrides using .htaccess           : Enable"
+  echo "| Click                                     : OK"
+  echo "| Click                                     : OK"
+  echo "| API key for SABnzbd                       : $INST_SABNZBD_KEY_API"
+  echo "-----------------------------------------------------------"
+  open /Applications/Server.app
+  echo " --- press any key to continue ---"
+  read -n 1 -s
 
   cd $DIR
+  printf "$PRINTF_MASK" "-> Spotweb installed" "$GREEN" "[OK]" "$RESET"
 fi
-
-##### TESTING #####
-exit 1
-##### TESTING #####
 
 #------------------------------------------------------------------------------
 # Install Spotweb
 #------------------------------------------------------------------------------
 #export PATH=$PATH:/usr/local/opt/postgresql/bin
 
-#sudo -u andries psql postgres -c "create database spotweb_db"
-#sudo -u andries psql postgres -c "create user spotweb_user with password 'spotweb_user'"
-#sudo -u andries psql postgres -c "grant all privileges on database spotweb_db to spotweb_user"
-
-sudo -u andries psql postgres -c "create database $INST_NEWZNAB_PSQL_DB"
-sudo -u andries psql postgres -c "create user $INST_NEWZNAB_PSQL_UID with password '"$INST_NEWZNAB_PSQL_PW"'"
-sudo -u andries psql postgres -c "grant all privileges on database $INST_NEWZNAB_PSQL_DB to $INST_NEWZNAB_PSQL_UID"
-
+#sudo -u andries psql postgres -c "create database $INST_NEWZNAB_PSQL_DB"
+#sudo -u andries psql postgres -c "create user $INST_NEWZNAB_PSQL_UID with password '"$INST_NEWZNAB_PSQL_PW"'"
+#sudo -u andries psql postgres -c "grant all privileges on database $INST_NEWZNAB_PSQL_DB to $INST_NEWZNAB_PSQL_UID"
 
 ###!/bin/bash
 ##mysql -u root -pSeCrEt << EOF
@@ -219,12 +251,11 @@ sudo -u andries psql postgres -c "grant all privileges on database $INST_NEWZNAB
 ##show tables;
 ##EOF
 
-
 ## mysql -h 192.168.1.10 -u root -pSeCrEt -e "show databases"
 
 ### ERROR:  syntax error at or near "'mini_spotweb'"
 ### sudo -u andries psql -c ALTER USER spotweb_usr SET PASSWORD 'mini_spotweb';
-sudo -u andries psql -c "ALTER USER $INST_NEWZNAB_PSQL_UID SET PASSWORD '"$INST_NEWZNAB_PSQL_PW"';"
+#sudo -u andries psql -c "ALTER USER $INST_NEWZNAB_PSQL_UID SET PASSWORD '"$INST_NEWZNAB_PSQL_PW"';"
 
 #?? open /Applications/pgAdmin3.app
 #?? echo "Open pgAdmin and set the password of user: spotweb_user"
@@ -269,84 +300,22 @@ sudo -u andries psql -c "ALTER USER $INST_NEWZNAB_PSQL_UID SET PASSWORD '"$INST_
 #echo "vi /var/www/spotweb/dbsettings.inc.php"
 #echo "You can now close the install website"
 
-echo "-----------------------------------------------------------"
-echo "| Paste the information as seen in the installer:"
-echo "| Type                          : PostgreSQL"
-echo "| Server                        : localhost"
-echo "| Database                      : $INST_NEWZNAB_PSQL_DB"
-echo "| Username                      : $INST_NEWZNAB_PSQL_UID"
-echo "| Password                      : $INST_NEWZNAB_PSQL_PW"
-echo "-----------------------------------------------------------"
-echo "| Usenet Server                 : $INST_NEWSSERVER_NAME"
-echo "| User Name                     : $INST_NEWSSERVER_SERVER_UID"
-echo "-----------------------------------------------------------"
-open http://localhost/spotweb/install.php
-echo " --- press any key to continue ---"
-read -n 1 -s
-
-echo "-----------------------------------------------------------"
-echo "| After finishing all steps, copy/paste the information as during the last phase"
-sudo touch $INST_SPOTWEB_PATH/dbsettings.inc.php
-sudo subl $INST_SPOTWEB_PATH/dbsettings.inc.php
-echo " --- press any key to continue ---"
-read -n 1 -s
 
 #/Library/WebServer/Documents/spotweb/retrieve.php
 #sh php /Library/WebServer/Documents/spotweb/retrieve.php
 #/bin/bash php /Library/WebServer/Documents/spotweb/retrieve.php
 
-echo "-----------------------------------------------------------"
-echo "| Click on the Cogwheel top-right and enter the following settings:"
-echo "| "
-echo "| NZB Handeling"
-echo "| What shall we do with NZB files           : Call SABnzbd through HTTP by Spotweb"
-echo "| What shall we do with multiple NZB files? : Merge NZB files"
-echo "| URL to SABnzbd                            : localhost:$INST_SABNZBD_PORT"
-echo "| API key for SABnzbd                       : $INST_SABNZBD_KEY_API"
-echo "-----------------------------------------------------------"
-echo "| Change"
-echo "-----------------------------------------------------------"
-open http://localhost/spotweb/install.php
-
-echo " --- press any key to continue ---"
-read -n 1 -s
 
 #osascript -e 'tell app "Terminal"
 #    do script "php /Users/Spotweb/Sites/spotweb/retrieve.php"
 #end tell'
 
-[ -d /usr/local/share/spotweb ] || mkdir -p /usr/local/share/spotweb
-cp /Users/Andries/Github/OSX_NewBox/bin/share/tmux_sync_spotweb.sh /usr/local/share/spotweb
-cp /Users/Andries/Github/OSX_NewBox/bin/share/spotweb_cycle.sh /usr/local/share/spotweb
-ln -s /usr/local/share/spotweb/tmux_sync_spotweb.sh /usr/local/bin/
-
-[ -d /var/log/spotweb ] || sudo mkdir -p /var/log/spotweb && sudo chown `whoami` /var/log/spotweb
-
-INST_FILE_LAUNCHAGENT="com.tmux.spotweb.plist"
-if [ -f $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT ] ; then
-    echo "Copying Lauch Agent file: $INST_FILE_LAUNCHAGENT"
-    cp $DIR/launchctl/$INST_FILE_LAUNCHAGENT ~/Library/LaunchAgents/
-    if [ "$?" != "0" ]; then
-        echo -e "${RED}  ============================================== ${RESET}"
-        echo -e "${RED} | ERROR ${RESET}"
-        echo -e "${RED} | Copy failed: ${RESET}"
-        echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
-        echo -e "${RED} | --- press any key to continue --- ${RESET}"
-        echo -e "${RED}  ============================================== ${RESET}"
-        read -n 1 -s
-        exit 1
-    fi
-    launchctl load ~/Library/LaunchAgents/$INST_FILE_LAUNCHAGENT
-else
-    echo -e "${RED}  ============================================== ${RESET}"
-    echo -e "${RED} | ERROR ${RESET}"
-    echo -e "${RED} | LaunchAgent file not found: ${RESET}"
-    echo -e "${RED} | $DIR/conf/launchctl/$INST_FILE_LAUNCHAGENT  ${RESET}"
-    echo -e "${RED} | --- press any key to continue --- ${RESET}"
-    echo -e "${RED}  ============================================== ${RESET}"
-    read -n 1 -s
-    exit 1
-fi
+#[ -d /usr/local/share/spotweb ] || mkdir -p /usr/local/share/spotweb
+#cp /Users/Andries/Github/OSX_NewBox/bin/share/tmux_sync_spotweb.sh /usr/local/share/spotweb
+#cp /Users/Andries/Github/OSX_NewBox/bin/share/spotweb_cycle.sh /usr/local/share/spotweb
+#ln -s /usr/local/share/spotweb/tmux_sync_spotweb.sh /usr/local/bin/
+#
+#[ -d /var/log/spotweb ] || sudo mkdir -p /var/log/spotweb && sudo chown `whoami` /var/log/spotweb
 
 #------------------------------------------------------------------------------
 # Install Spotweb - Complete
@@ -372,4 +341,23 @@ fi
 ## php /var/www/spotweb/retrieve.php
 ## /usr/share/spotweb/scripts/retrieve.sh
 
-
+#################### 
+## http://www.macminivault.com/mysql-mavericks/
+## https://github.com/CiTroNaK/Installation-guide-for-GitLab-on-OS-X
+#################### 
+#/usr/bin/mysqladmin ping| grep 'mysqld is alive' > /dev/null 2>&1
+#if [ $? != 0 ]
+#then
+#  /etc/init.d/mysqld stop
+#  /etc/init.d/mysqld start
+#    echo Starting at `date`
+#fi
+#################### 
+#MYSQL_UP=$(pgrep mysql | wc -l);
+#if [ "$MYSQL_UP" -ne 1 ]; then
+#  echo "MySQL is down."
+#  #sudo service mysql start
+#else
+#  echo "MySQL is up."
+#fi
+#################### 
