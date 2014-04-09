e306f6b6c02e72d6cc6b65ca2d4b5233582d1f4a
ajongsma <a.jongsma@gmail.com>
2014-04-09 17:49:00 +0200

    Update install_test.sh

--- install_test.sh
+++ install_test.sh
@@ -95,7 +95,12 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #------------------------------------------------------------------------------
 # Install testing launcher
 #------------------------------------------------------------------------------
-if [[ $INST_SPOTWEB == "true" ]]; then
-  source "$DIR/scripts/osx_php_gettext.sh"
-  source "$DIR/scripts/install_spotweb.sh"
+### SPOTWEB - Halted due to unresolved error of application
+#if [[ $INST_SPOTWEB == "true" ]]; then
+#  source "$DIR/scripts/osx_php_gettext.sh"
+#  source "$DIR/scripts/install_spotweb.sh"
+#fi
+
+if [[ $INST_MACPAR == "true" ]]; then
+  source "$DIR/scripts/install_macpar.sh"
 fi
