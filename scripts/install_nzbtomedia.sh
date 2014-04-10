5e6acdfe3763940c4438b853d5f801ef24fd2aee
ajongsma <a.jongsma@gmail.com>
2014-04-10 18:54:36 +0200

    Update install_nzbtomedia.sh

--- scripts/install_nzbtomedia.sh
+++ scripts/install_nzbtomedia.sh
@@ -47,41 +47,41 @@ else
     printf "$PRINTF_MASK" "nzbToMedia detected" "$GREEN" "[OK]" "$RESET"
   fi
 
-  if [ ! -f ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg ] ; then
-    printf "$PRINTF_MASK" "autoProcessMedia.cfg not detected, creating..." "$YELLOW" "[WAIT]" "$RESET"
-    
-    cp $INST_NZBTOMEDIA_PATH/nzbToMedia/autoProcessMedia.cfg.sample ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
-    echo "-----------------------------------------------------------"
-    echo "| Modify the following:"
-    echo "-----------------------------------------------------------"
-    echo "| [CouchPotato]"
-    echo "| apikey    =  $INST_COUCHPOTATO_KEY_API"
-    echo "| port      =  $INST_COUCHPOTATO_PORT"
-    echo "| "
-    echo "| [SickBeard]"
-    echo "| port      =  $INST_SICKBEARD_PORT"
-    echo "| username  = $INST_SICKBEARD_UID"
-    echo "| password  = $INST_SICKBEARD_PW"
-    echo "-----------------------------------------------------------"
-    echo "| Save"
-    echo "-----------------------------------------------------------"
-    if [ ! -d /Applications/TextWrangler.app ]; then
-      pico ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
-    else
-      open -a /Applications/TextWrangler.app ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
-    fi
-    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-    read -n 1 -s
-  else
-    printf "$PRINTF_MASK" "autoProcessMedia.cfg detected" "$GREEN" "[OK]" "$RESET"
-  fi
-
-  if [ ! -h ~/Library/Application\ Support/SABnzbd/scripts/nzbToCouchPotato.py ]; then
-    printf "$PRINTF_MASK" "Symbolic link nzbToCouchPotato.py not detected, creating..." "$YELLOW" "[WAIT]" "$RESET"
-    sudo ln -sfv $INST_NZBTOMEDIA_PATH/nzbToMedia/nzbToCouchPotato.py ~/Library/Application\ Support/SABnzbd/scripts/nzbToCouchPotato.py
-  else
-    printf "$PRINTF_MASK" "Symbolic link nzbToCouchPotato.py detected" "$GREEN" "[OK]" "$RESET"
-  fi
+#  if [ ! -f ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg ] ; then
+#    printf "$PRINTF_MASK" "autoProcessMedia.cfg not detected, creating..." "$YELLOW" "[WAIT]" "$RESET"
+#    
+#    cp $INST_NZBTOMEDIA_PATH/nzbToMedia/autoProcessMedia.cfg.sample ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
+#    echo "-----------------------------------------------------------"
+#    echo "| Modify the following:"
+#    echo "-----------------------------------------------------------"
+#    echo "| [CouchPotato]"
+#    echo "| apikey    =  $INST_COUCHPOTATO_KEY_API"
+#    echo "| port      =  $INST_COUCHPOTATO_PORT"
+#    echo "| "
+#    echo "| [SickBeard]"
+#    echo "| port      =  $INST_SICKBEARD_PORT"
+#    echo "| username  = $INST_SICKBEARD_UID"
+#    echo "| password  = $INST_SICKBEARD_PW"
+#    echo "-----------------------------------------------------------"
+#    echo "| Save"
+#    echo "-----------------------------------------------------------"
+#    if [ ! -d /Applications/TextWrangler.app ]; then
+#      pico ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
+#    else
+#      open -a /Applications/TextWrangler.app ~/Library/Application\ Support/SABnzbd/scripts/autoProcessMedia.cfg
+#    fi
+#    echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+#    read -n 1 -s
+#  else
+#    printf "$PRINTF_MASK" "autoProcessMedia.cfg detected" "$GREEN" "[OK]" "$RESET"
+#  fi
+#
+#  if [ ! -h ~/Library/Application\ Support/SABnzbd/scripts/nzbToCouchPotato.py ]; then
+#    printf "$PRINTF_MASK" "Symbolic link nzbToCouchPotato.py not detected, creating..." "$YELLOW" "[WAIT]" "$RESET"
+#    sudo ln -sfv $INST_NZBTOMEDIA_PATH/nzbToMedia/nzbToCouchPotato.py ~/Library/Application\ Support/SABnzbd/scripts/nzbToCouchPotato.py
+#  else
+#    printf "$PRINTF_MASK" "Symbolic link nzbToCouchPotato.py detected" "$GREEN" "[OK]" "$RESET"
+#  fi
 
   cd $DIR
 fi
