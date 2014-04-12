a07bc53d116d91e5beee96cef92aeb31a0bfef73
ajongsma <a.jongsma@gmail.com>
2014-04-12 12:44:34 +0200

    Create osx_php_xdebug.sh

---
+++ scripts/osx_php_xdebug.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo touch /Library/Server/Web/Config/php/php_xdebug.ini
+echo "Don't forget to add 'zend_extension=/usr/lib/php/extensions/no-debug-non-zts-20100525/xdebug.so' to /Library/Server/Web/Config/php/php_xdebug.ini"
+#sudo echo "zend_extension=/usr/lib/php/extensions/no-debug-non-zts-20100525/xdebug.so" > /Library/Server/Web/Config/php/php_xdebug.ini
+echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+read -n 1 -s
+
+sudo apachectl graceful
