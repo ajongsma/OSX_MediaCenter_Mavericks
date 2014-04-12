e08c0cf173f454a0e390938f64caeba263a11073
ajongsma <a.jongsma@gmail.com>
2014-04-12 13:20:12 +0200

    Update osx_php_pear.sh

--- scripts/osx_php_pear.sh
+++ scripts/osx_php_pear.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 source config.sh
 
+cd /usr/lib/php
+#sudo php -d detect_unicode=0 go-pear.phar
+#sudo php install-pear-nozlib.phar
+sudo php -d detect_unicode=0 install-pear-nozlib.phar
 
+# include_path = ".:/usr/lib/php/:/usr/lib/php/pear/"
+echo "-----------------------------------------------------------"
+echo "| /etc/php.ini"
+echo "| - Section Paths and Directories"
+echo "-----------------------------------------------------------"
+echo "| Add               : include_path = \".:/usr/lib/php/:/usr/lib/php/pear/\""
+echo "-----------------------------------------------------------"
+echo "| Save changes"
+echo "-----------------------------------------------------------"
+if [ ! -d /Applications/TextWrangler.app ]; then
+  sudo pico /etc/php.ini
+else
+  sudo open -a /Applications/TextWrangler.app /etc/php.ini
+fi
+echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+read -n 1 -s
+
+sudo apachectl graceful
+
+
+cd $DIR
