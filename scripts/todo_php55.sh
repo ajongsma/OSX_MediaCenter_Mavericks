698158ba69c1bd83a4a9b4af85a5a1d1f669163b
ajongsma <a.jongsma@gmail.com>
2014-04-16 19:49:02 +0200

    Update todo_php55.sh

--- scripts/todo_php55.sh
+++ scripts/todo_php55.sh
@@ -12,3 +12,10 @@ brew install tidy
 #brew install php55 --with-pgsql --with-mysql --with-tidy --with-intl --with-gmp
 brew install php55 --with-mysql --with-tidy --with-intl --with-gmp
 
+#/usr/local/etc/php/5.5/php.ini
+
+brew install php55-intl
+
+
+#ln -sfv /usr/local/opt/php55/*.plist ~/Library/LaunchAgents
+#launchctl load ~/Library/LaunchAgents/homebrew.mxcl.php55.plist
