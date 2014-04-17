2065169623467245649a5a8417547442333452ae
ajongsma <a.jongsma@gmail.com>
2014-04-17 18:59:58 +0200

    Update osx_php_pear.sh

--- scripts/osx_php_pear.sh
+++ scripts/osx_php_pear.sh
@@ -12,20 +12,20 @@ sudo pecl channel-update pecl.php.net
 sudo pear upgrade-all
 sudo pear config-set auto_discover 1
 
-# include_path = ".:/usr/lib/php/:/usr/lib/php/pear/"
-echo "-----------------------------------------------------------"
-echo "| /etc/php.ini"
-echo "| - Section Paths and Directories"
-echo "-----------------------------------------------------------"
-echo "| Add               : include_path = \".:/usr/lib/php/:/usr/lib/php/pear/\""
-echo "-----------------------------------------------------------"
-echo "| Save changes"
-echo "-----------------------------------------------------------"
-osascript -e 'tell app "Terminal"
-      do script "sudo pico /etc/php.ini"
-  end tell'
-echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-read -n 1 -s
+## include_path = ".:/usr/lib/php/:/usr/lib/php/pear/"
+#echo "-----------------------------------------------------------"
+#echo "| /etc/php.ini"
+#echo "| - Section Paths and Directories"
+#echo "-----------------------------------------------------------"
+#echo "| Add               : include_path = \".:/usr/lib/php/:/usr/lib/php/pear/\""
+#echo "-----------------------------------------------------------"
+#echo "| Save changes"
+#echo "-----------------------------------------------------------"
+#osascript -e 'tell app "Terminal"
+#      do script "sudo pico /etc/php.ini"
+#  end tell'
+#echo -e "${BLUE} --- press any key to continue --- ${RESET}"
+#read -n 1 -s
 
 sudo apachectl graceful
 
