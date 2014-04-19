90545685ba84567f9f7869e52d49fbc327444002
ajongsma <a.jongsma@gmail.com>
2014-04-19 20:35:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -295,4 +295,5 @@ fi
 if [[ $INST_SPOTWEB == "true" ]]; then
   source "$DIR/scripts/osx_php55.sh"
   source "$DIR/scripts/install_spotweb.sh"
+  source "$DIR/scripts/install_spotweb_sabnzbd.sh"
 fi
