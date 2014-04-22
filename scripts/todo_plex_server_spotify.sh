f118e8db45d11d34dd347435c698602271558714
ajongsma <a.jongsma@gmail.com>
2014-04-22 10:11:23 +0200

    Update todo_plex_server_spotify.sh

--- scripts/todo_plex_server_spotify.sh
+++ scripts/todo_plex_server_spotify.sh
@@ -2,6 +2,23 @@
 source config.sh
 
 function check_config_defaults() {
+  if [[ -z $INST_PLEX_SPOTIFY_PATH ]]; then
+    printf 'One or more values were not detected in the config.sh, please add the appropriate values:\n' "YELLOW" $col '[WAIT]' "$RESET"
+    echo "| Plex Spotify Path             : INST_PLEX_SPOTIFY_PATH"
+    if [ ! -d /Applications/TextWrangler.app ]; then
+      pico config.sh
+    else
+      open -a /Applications/TextWrangler.app config.sh
+    fi
+    while ( [[ $INST_PLEX_SPOTIFY_PATH == "" ]] )
+    do
+      printf '.'
+      sleep 2
+      source config.sh
+    done
+    printf "$PRINTF_MASK" ".\n" "$GREEN" "[OK]" "$RESET"
+  fi
+  
   if [ ! -e /usr/local/bin/brew ] ; then
     printf "$PRINTF_MASK" "Homebrew not detected" "$RED" "[ERR]" "$RESET"
     echo -e "${BLUE} --- press any key to continue --- ${RESET}"
@@ -10,10 +27,7 @@ function check_config_defaults() {
   fi
 }
 
-
 #INST_PLEX_SPOTIFY_PATH="/Users/Plex"
-
-
 ## https://github.com/Burgestrand/spotify/wiki
 
 if [ -h /Library/Application Support/Plex Media Server/Plug-ins/Spotify.bundle ] ; then
@@ -27,6 +41,24 @@ else
     printf "$PRINTF_MASK" "Old Spotify bundle not detected" "$GREEN" "[OK]" "$RESET"
   fi
   brew install libspotify
+  
+  if [ ! -d $INST_PLEX_SPOTIFY_PATH ] ; then
+    printf "$PRINTF_MASK" $INST_PLEX_SPOTIFY_PATH" doesn't exists, creating..." "$YELLOW" "[WAIT]" "$RESET"
+    sudo mkdir -p $INST_PLEX_SPOTIFY_PATH
+    sudo chown `whoami`:staff $INST_PLEX_SPOTIFY_PATH
+  else
+    printf "$PRINTF_MASK" $INST_PLEX_SPOTIFY_PATH" exists" "$GREEN" "[OK]" "$RESET"
+  fi
+
+  cd $INST_PLEX_SPOTIFY_PATH
+  if [ ! -d $INST_PLEX_SPOTIFY_PATH/Spotify.bundle ] ; then
+    printf "$PRINTF_MASK" "Spotify.bundle doesn't exists, downloading respository..." "$YELLOW" "[WAIT]" "$RESET"
+    #git clone https://github.com/ljunkie/plexWatch.git
+  else
+    printf "$PRINTF_MASK" "Spotify.bundle exists" "$GREEN" "[OK]" "$RESET"
+  fi
+  
+  
 fi
 
 
