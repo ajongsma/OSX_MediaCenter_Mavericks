83003840968a5a138ce9bd26fbc3a5bd6f8239a0
ajongsma <a.jongsma@gmail.com>
2014-04-22 09:58:46 +0200

    Update todo_plex_server_spotify.sh

--- scripts/todo_plex_server_spotify.sh
+++ scripts/todo_plex_server_spotify.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 source config.sh
 
-
-if [ ! -e /Applications/Plex\ Media\ Server.app ] ; then
-  printf "$PRINTF_MASK" "Plex Media Server not detected" "$RED" "[FAIL]" "$RESET"
-  echo -e "${BLUE} --- press any key to continue --- ${RESET}"
-  read -n 1 -s
-  exit 1
-else
-  printf "$PRINTF_MASK" "Plex Media Server detected" "$GREEN" "[OK]" "$RESET"
-fi
-
-
-if [ -f libspotify ] ; then
-  printf "$PRINTF_MASK" "libspotify detected" "$GREEN" "[OK]" "$RESET"
-else
-  printf "$PRINTF_MASK" "libspotify not detected, installing..." "$YELLOW" "[WAIT]" "$RESET"
+function check_config_defaults() {
   if [ ! -e /usr/local/bin/brew ] ; then
     printf "$PRINTF_MASK" "Homebrew not detected" "$RED" "[ERR]" "$RESET"
     echo -e "${BLUE} --- press any key to continue --- ${RESET}"
     read -n 1 -s
     exit 1
-  else
-    brew install libspotify
   fi
+}
+
+## https://github.com/Burgestrand/spotify/wiki
+
+if [ -h /Library/Application Support/Plex Media Server/Plug-ins/Spotify.bundle ] ; then
+  printf "$PRINTF_MASK" "Spotify bundle (symlinked) detected" "$GREEN" "[OK]" "$RESET"
+else
+  check_config_defaults
+  
+  if [ -d /Library/Application Support/Plex Media Server/Plug-ins/Spotify.bundle ] ; then
+    printf "$PRINTF_MASK" "Old Spotify bundle detected" "$YELLOW" "[WAIT]" "$RESET"
+  else
+    printf "$PRINTF_MASK" "Old Spotify bundle not detected" "$GREEN" "[OK]" "$RESET"
+  fi
+  brew install libspotify
 fi
+
 
