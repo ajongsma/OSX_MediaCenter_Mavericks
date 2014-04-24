fca9fa7883f9d5ee4a3bd71188b5fff613b6d551
ajongsma <a.jongsma@gmail.com>
2014-04-24 20:47:39 +0200

    Update install_plex_server_maraschino.sh

--- scripts/install_plex_server_maraschino.sh
+++ scripts/install_plex_server_maraschino.sh
@@ -48,5 +48,13 @@ else
     git pull https://github.com/gugahoi/maraschino.git
   fi
   
+  if [ ! -h $INST_APACHE_SYSTEM_WEB_ROOT/maraschino ]; then
+    printf "$PRINTF_MASK" "Symbolic link not detected, creating..." "$YELLOW" "[WAIT]" "$RESET"
+    sudo ln -sfv $INST_MARASCHINO_PATH/maraschino /Library/Server/Web/Data/Sites/Default/maraschino
+    #sudo ln -s $INST_SPOTWEB_PATH/Sites/spotweb /Library/Server/Web/Data/Sites/Default/spotweb
+  else
+    printf "$PRINTF_MASK" "Symbolic link detected" "$GREEN" "[OK]" "$RESET"
+  fi
+  
   
 fi
