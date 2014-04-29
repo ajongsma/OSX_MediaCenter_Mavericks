3acc80e5087f898f0c193509cdd5a1cc9a0f0986
ajongsma <a.jongsma@gmail.com>
2014-04-29 23:15:31 +0200

    Update todo_osx_terminal_colors.sh

--- scripts/todo_osx_terminal_colors.sh
+++ scripts/todo_osx_terminal_colors.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 source config.sh
 
-echo # This command tells termical to add color >> ~/.bash_profile
-echo export CLICOLOR=1 >> ~/.bash_profile
-echo export LSCOLORS=ExFxCxDxBxegedabagacad >> ~/.bash_profile
+echo "# Add color to the terminal" >> ~/.bash_profile
+echo "export CLICOLOR=1" >> ~/.bash_profile
+echo "export LSCOLORS=ExFxCxDxBxegedabagacad" >> ~/.bash_profile
 echo ""  >> ~/.bash_profile
-echo # This command changes the terminal login colors >> ~/.bash_profile
-echo PS1=’\[\e[0;33m\]\h:\W \u\$\[\e[m\] ‘ >> ~/.bash_profile
+
+#echo "# Changes the terminal login colors" >> ~/.bash_profile
+#echo "PS1=’\[\e[0;33m\]\h:\W \u\$\[\e[m\] ‘" >> ~/.bash_profile
 
 brew install grc
+echo ""  >> ~/.bash_profile
+echo "# Add grc to the terminal" >> ~/.bash_profile
 echo source "`brew --prefix`/etc/grc.bashrc" >> ~/.bash_profile
 
 ## http://noahfrederick.com/blog/2011/lion-terminal-theme-peppermint/
